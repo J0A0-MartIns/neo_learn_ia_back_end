e40725605b17a9dfb76c896921bda4ae773bfe00
João Marcos Martins <135861665+J0A0-MartIns@users.noreply.github.com>
2025-09-10 19:29:28 -0300

    Projeto inicial

---
+++ src/main/java/com/neolearnia/NeoLearnIaApplication.java
@@ -0,0 +1,13 @@
+package com.neolearnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoLearnIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoLearnIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neolearnia/NeoLearnIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neolearnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoLearnIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
